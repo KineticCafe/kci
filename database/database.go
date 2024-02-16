f5f6abfee36ed94c38495460984695972678e027
Trevor Oke <trevor@trevoroke.com>
2024-02-16 15:48:11 -0500

    List RDS Snapshots

--- database/database.go
+++ database/database.go
@@ -76,29 +76,47 @@ func (mgr *RDSManager) Fetch(filter string) error {
 			MultiAZ:          *dbInstance.MultiAZ,
 		}
 
-		snapshotsResp, err := mgr.Client.DescribeDBSnapshots(context.TODO(), &rds.DescribeDBSnapshotsInput{
-			DBInstanceIdentifier: dbInstance.DBInstanceIdentifier,
-		})
+		snapshots, err := mgr.FetchSnapshots(dbInfo.ID)
 		if err != nil {
-			return fmt.Errorf("Unable to list DB snapshots for %s: %v", *dbInstance.DBInstanceIdentifier, err)
+			return fmt.Errorf("could not load snapshots: %v", err)
 		}
 
-		for _, snapshot := range snapshotsResp.DBSnapshots {
-			snapshotInfo := SnapshotInfo{
-				ID:      *snapshot.DBSnapshotIdentifier,
-				Created: *snapshot.SnapshotCreateTime,
-			}
-			dbInfo.Snapshots = append(dbInfo.Snapshots, snapshotInfo)
-		}
-
-		sort.Slice(dbInfo.Snapshots, func(i, j int) bool {
-			return dbInfo.Snapshots[i].Created.Before(dbInfo.Snapshots[j].Created)
-		})
+		dbInfo.Snapshots = snapshots
 
 		mgr.Databases = append(mgr.Databases, dbInfo)
 	}
 
 	return nil
+}
+
+func (mgr *RDSManager) FetchSingle(identifier string) ([]DatabaseInfo, error) {
+	return []DatabaseInfo{}, nil
+}
+
+func (mgr *RDSManager) FetchSnapshots(identifier string) ([]SnapshotInfo, error) {
+	snapshots := []SnapshotInfo{}
+
+	snapshotsResp, err := mgr.Client.DescribeDBSnapshots(context.TODO(), &rds.DescribeDBSnapshotsInput{
+		DBInstanceIdentifier: aws.String(identifier),
+	})
+
+	if err != nil {
+		return snapshots, fmt.Errorf("Unable to list DB snapshots for %s: %v", identifier, err)
+	}
+
+	for _, snapshot := range snapshotsResp.DBSnapshots {
+		snapshotInfo := SnapshotInfo{
+			ID:      *snapshot.DBSnapshotIdentifier,
+			Created: *snapshot.SnapshotCreateTime,
+		}
+		snapshots = append(snapshots, snapshotInfo)
+	}
+
+	sort.Slice(snapshots, func(i, j int) bool {
+		return snapshots[i].Created.Before(snapshots[j].Created)
+	})
+
+	return snapshots, nil
 }
 
 func (db *DatabaseInfo) LatestSnapshot() (SnapshotInfo, error) {
@@ -120,99 +138,3 @@ func (db *DatabaseInfo) LatestSnapshotID() string {
 
 	return ""
 }
-
-// FetchAMIAge retrieves the AMI age of the given EC2 instance.
-/*
-func (instance *EC2Instance) FetchAMIAge(client *ec2.Client) error {
-	describeImagesInput := &ec2.DescribeImagesInput{
-		ImageIds: []string{instance.AMI_ID},
-	}
-
-	imagesResp, err := client.DescribeImages(context.Background(), describeImagesInput)
-	if err != nil {
-		return fmt.Errorf("failed to describe image %s, %v", instance.AMI_ID, err)
-	}
-
-	if len(imagesResp.Images) == 0 {
-		return fmt.Errorf("no image found with id %s", instance.AMI_ID)
-	}
-
-	amiCreationDate, err := time.Parse(time.RFC3339, *imagesResp.Images[0].CreationDate)
-	if err != nil {
-		return fmt.Errorf("failed to parse creation date of image %s, %v", instance.AMI_ID, err)
-	}
-
-	instance.AMI_Age = fmt.Sprintf("%d", int(time.Since(amiCreationDate).Hours()/24))
-
-	return nil
-}
-
-// FetchAMIAge recursively calls FetchAMIAge on all Instances.
-func (mgr *EC2InstanceManager) FetchAMIAge() error {
-	for i := range mgr.Instances {
-		err := mgr.Instances[i].FetchAMIAge(mgr.Client)
-		if err != nil {
-			return fmt.Errorf("unable to scan AMI Ages: %w", err)
-		}
-	}
-
-	return nil
-}
-
-// FetchInstances connects to an AWS and fetches descriptions of all
-// ec2 instances. These instances will be available in the Instances
-// field.
-func (mgr *EC2InstanceManager) FetchInstances(filter string) error {
-	// empty in case of multiple runs
-	mgr.Instances = []EC2Instance{}
-
-	var filters []types.Filter
-	if filter != "" {
-		filter = "*" + filter + "*"
-
-		filters = append(filters, types.Filter{
-			Name:   aws.String("tag:Name"),
-			Values: []string{filter},
-		})
-	}
-
-	input := &ec2.DescribeInstancesInput{
-		Filters: filters,
-	}
-
-	resp, err := mgr.Client.DescribeInstances(context.Background(), input)
-	if err != nil {
-		return fmt.Errorf("failed to describe instances: %w", err)
-	}
-
-	for _, reservation := range resp.Reservations {
-		for _, instance := range reservation.Instances {
-			var name string
-
-			for _, tag := range instance.Tags {
-				if *tag.Key == "Name" {
-					name = *tag.Value
-					break
-				}
-			}
-
-			instanceStruct := EC2Instance{
-				ID:          *instance.InstanceId,
-				Name:        name,
-				AMI_ID:      *instance.ImageId,
-				InstanceAge: fmt.Sprintf("%d", int(time.Since(*instance.LaunchTime).Hours()/24)),
-				AMI_Age:     "N/A", // not scanned initially
-				IsSSM:       false, // This needs actual check
-				Status:      string(instance.State.Name),
-				PublicIP:    aws.ToString(instance.PublicIpAddress),
-				PrivateIP:   aws.ToString(instance.PrivateIpAddress),
-			}
-			mgr.Instances = append(mgr.Instances, instanceStruct)
-		}
-	}
-
-	return nil
-}
-
-//manager.Filter(ec2_instance.RunningInstances)
-*/
